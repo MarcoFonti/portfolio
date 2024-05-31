b4283223f8364f647569bec7872ab04e4b9444a7
MarcoFonti <theemporiumg@gmail.com>
2024-05-31 17:38:36 +0200

    add photo-skillis

---
+++ src/assets/data/_DataMySkill.js
@@ -0,0 +1,75 @@
+/* DATI SKILLIS */
+export default [
+    {
+        title: 'Git',
+        image: '../img/git-removebg-preview.png'
+    },
+    {
+        title: 'GitHub',
+        image: '../img/github-removebg-preview.png'
+    },
+    {
+        title: 'HTML',
+        image: '../img/html-removebg-preview.png'
+    },
+    {
+        title: 'CSS',
+        image: '../img/css-removebg-preview.png'
+    },
+    {
+        title: 'Bootstrap',
+        image: '../img/bootstrap-removebg-preview.png'
+    },
+    {
+        title: 'JavaScript',
+        image: '../img/js-removebg-preview.png'
+    },
+    {
+        title: 'Vue',
+        image: '../img/vue-removebg-preview.png'
+    },
+    {
+        title: 'Vite',
+        image: '../img/vite-removebg-preview.png'
+    },
+    {
+        title: 'Sass',
+        image: '../img/sass-removebg-preview.png'
+    },
+    {
+        title: 'PHP',
+        image: '../img/php-removebg-preview.png'
+    },
+    {
+        title: 'SQL',
+        image: '../img/sql-removebg-preview.png'
+    },
+    {
+        title: 'MySQL',
+        image: '../img/my-sql-removebg-preview.png'
+    },
+    {
+        title: 'phpMyAdmin',
+        image: '../img/phpmyadmin-removebg-preview.png'
+    },
+    {
+        title: 'Laravel',
+        image: '../img/laravel-removebg-preview.png'
+    },
+    {
+        title: 'API',
+        image: '../img/api-removebg-preview.png'
+    },
+    {
+        title: 'WordPress',
+        image: '../img/wordpress-removebg-preview.png'
+    },
+    {
+        title: 'Shopify',
+        image: '../img/shopify-removebg-preview.png'
+    },
+    {
+        title: 'Canva',
+        image: '../img/canva-removebg-preview.png'
+    }
+];
